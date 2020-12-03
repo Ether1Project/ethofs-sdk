1720397bdee3ebf05fba7833cd595910edd8a467
James Reilly <admin@ether1.org>
2020-12-03 12:11:19 -0800

    ethoFS Updates

--- src/commands/data/userPinnedDataTotal.js
+++ src/commands/data/userPinnedDataTotal.js
@@ -2,32 +2,10 @@ import axios from 'axios';
 import { baseUrl } from './../../constants';
 import { validateApiKeys } from '../../util/validators';
 
-export default function userPinnedDataTotal(pinataApiKey, pinataSecretApiKey) {
-    validateApiKeys(pinataApiKey, pinataSecretApiKey);
-
-    let endpoint = `${baseUrl}/data/userPinnedDataTotal`;
+export default function userPinnedDataTotal(ethofsKey) {
+    validateApiKeys(ethofsKey);
 
     return new Promise((resolve, reject) => {
-        axios.get(
-            endpoint,
-            {
-                withCredentials: true,
-                headers: {
-                    'pinata_api_key': pinataApiKey,
-                    'pinata_secret_api_key': pinataSecretApiKey
-                }
-            }).then(function (result) {
-            if (result.status !== 200) {
-                reject(new Error(`unknown server response while attempting to retrieve pinned data total: ${result}`));
-            }
-            resolve(result.data);
-        }).catch(function (error) {
-            //  handle error here
-            if (error && error.response && error.response && error.response.data && error.response.data.error) {
-                reject(new Error(error.response.data.error));
-            } else {
-                reject(error);
-            }
-        });
+        reject(new Error(`ethoFS pinned data retrieval not yet functional`));
     });
 }

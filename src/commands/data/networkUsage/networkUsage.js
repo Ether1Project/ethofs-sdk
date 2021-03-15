534229167c7d9a974f25570d4e3dc0e40f0c6274
Dev-JamesR <admin@ether1.org>
2021-03-15 16:44:35 +0000

    Network Usage Updates

--- src/commands/data/networkUsage/networkUsage.js
+++ src/commands/data/networkUsage/networkUsage.js
@@ -6,24 +6,41 @@ export default function networkUsage() {
 var web3 = new Web3(`${baseUrl}`);
 
     return new Promise((resolve, reject) => {
-
+        //console.log('Entering Promise');
         web3.eth.net.isListening()
             .then(function () {
-
             var ethofsContract = new web3.eth.Contract(pinStorageABI, pinStorageContractAddress);
+            var totalNetworkStorageUsed = 0;
+            var pinCount;
+            var responseCount = 0;
+            var i;
 
-            ethofsContract.methods.PinCount.call(function (error, pinCount) {
-                if (!error) {
-                    if (pinCount) {
-                        resolve({
-                            activeUploadContracts: pinCount
+            //console.log('ETHO RPC Is Listening');
+            //console.log('Contract Found');
+            ethofsContract.methods.PinCount().call().then(response => {
+                //console.log(response);
+                if (response) {
+                    pinCount = response;
+                    for (i = 0; i < pinCount; i++) {
+                        ethofsContract.methods.Pins(i).call().then(response => {
+                            ethofsContract.methods.GetPinSize(response).call().then(response => {
+                                totalNetworkStorageUsed += response;
+                                responseCount++;
+                                if (responseCount >= (pinCount - 1)) {
+                                    resolve({
+                                        activeUploadContracts: pinCount,
+                                        totalNetworkStorageUsed: totalNetworkStorageUsed
+                                    });
+                                }
+                            });
                         });
-                    } else {
-                        reject(error);
                     }
                 } else {
-                    reject(error);
+                    console.log('Error retrieving contract data');
+                    reject('Error retrieving contract data');
                 }
+            }).catch(function (error) {
+                    reject(error);
             });
         }).catch(function (error) {
             reject(error);
